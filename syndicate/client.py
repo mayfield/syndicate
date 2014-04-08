f2069e8d6989812adeca5b6012467bcd067d8097
Justin Mayfield <tooker@gmail.com>
2014-04-08 08:30:50 -0600

    Paging support and fix for loose datetime match.

--- syndicate/client.py
+++ syndicate/client.py
@@ -1,5 +1,5 @@
 '''
-Blocking client for REST apis.
+Client for REST apis.
 '''
 
 from __future__ import print_function, division
@@ -37,16 +37,29 @@ class LoginAuth(requests.auth.AuthBase):
 class Service(object):
     """ A stateful connection to a service. """
 
+    @staticmethod
+    def default_data_getter(x):
+        return x['data']
+
+    @staticmethod
+    def default_meta_getter(x):
+        return x['meta']
+
+    @staticmethod
+    def default_next_page_getter(x):
+        return x['meta']['next']
+
     def __init__(self, uri=None, urn=None, auth=None, serializer='json',
-                 data_getter=None, meta_getter=None, trailing_slash=True):
+                 data_getter=None, meta_getter=None, next_page_getter=None,
+                 trailing_slash=True):
         if not (uri and urn):
             raise TypeError("Required: uri, urn")
-        self.uri = uri
+        self.uri = uri.rstrip('/')
         self.urn = urn
         self.trailing_slash = trailing_slash
-        self.url = '%s/%s' % (uri.strip('/'), urn.strip('/'))
-        self.data_getter = data_getter
-        self.meta_getter = meta_getter
+        self.data_getter = data_getter or self.default_data_getter
+        self.meta_getter = meta_getter or self.default_meta_getter
+        self.next_page_getter = next_page_getter or self.default_next_page_getter
         self.session = requests.Session()
         if hasattr(serializer, 'mime'):
             self.serializer = serializer
@@ -58,43 +71,52 @@ class Service(object):
     def import_filter(self, root):
         """ Flatten a response with meta and data keys into an single object
         where the values in the meta dict are converted to attrs. """
-        data = self.data_getter(root)
-        if isinstance(data, dict):
-            data = data.DictResponse(data)
-        elif isinstance(root, list):
-            data = data.ListResponse(data)
+        result = self.data_getter(root)
+        if isinstance(result, dict):
+            result = data.DictResponse(result)
+        elif isinstance(result, list):
+            result = data.ListResponse(result)
+            result.next_page = self.next_page_getter(root)
         else:
-            return data
+            return result
         meta = self.meta_getter(root)
-        for mkey, mval in meta:
-            setattr(data, mkey, mval)
-        return data
+        for mkey, mval in meta.items():
+            setattr(result, mkey, mval)
+        return result
 
-    def do(self, method, *path, **query):
+    def do(self, method, path, urn=None, **query):
         path = tuple(x.strip('/') for x in path)
-        if self.trailing_slash:
+        if path and self.trailing_slash:
             path += ('',)
-        d = self.session.request(method, '/'.join((self.url,) + path),
+        urn = self.urn if urn is None else urn
+        url = '%s/%s' % (self.uri, urn.strip('/'))
+        d = self.session.request(method, '/'.join((url,) + path),
                                  params=query)
         d = self.serializer.decode(d.text)
         return self.import_filter(d)
 
     def get(self, *path, **query):
-        return self.do('get', *path, **query)
+        return self.do('get', path, **query)
 
     def get_pager(self, *path, **query):
+        page = self.get(*path, **query)
+        for x in page:
+            yield x
+        while page.next_page:
+            page = self.get(urn=page.next_page)
+            for x in page:
+                yield x
+
+    def post(self, *path, **query):
         raise NotImplementedError()
 
-    def post(self, method, *path, **query):
+    def delete(self, *path, **query):
         raise NotImplementedError()
 
-    def delete(self, method, *path, **query):
+    def put(self, *path, **query):
         raise NotImplementedError()
 
-    def put(self, method, *path, **query):
-        raise NotImplementedError()
-
-    def patch(self, method, *path, **query):
+    def patch(self, *path, **query):
         raise NotImplementedError()
 
 
--- syndicate/data.py
+++ syndicate/data.py
@@ -8,6 +8,7 @@ import collections
 import datetime
 import dateutil.parser
 import json
+import re
 
 Serializer = collections.namedtuple('Serializer', 'mime, encode, decode')
 
@@ -33,17 +34,17 @@ class NormalJSONEncoder(json.JSONEncoder):
 class NormalJSONDecoder(json.JSONDecoder):
     """ Normal == iso datatime. """
 
+    strict_iso_match = re.compile('\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}\:\d{2}.*)?$')
+
     def __init__(self):
         super(NormalJSONDecoder, self).__init__(object_hook=self.parse_object)
 
     def parse_object(self, data):
         """ Look for datetime looking strings. """
         for key, value in data.items():
-            try:
-                # XXX Too open.
+            if isinstance(value, (str, type(u''))) and \
+               self.strict_iso_match.match(value):
                 data[key] = dateutil.parser.parse(value)
-            except (AttributeError, TypeError, ValueError):
-                pass
         return data
 
 serializers = {
