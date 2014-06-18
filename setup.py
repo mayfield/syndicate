40a2a4ace817f3f237d87d802d5bd286eb2bf09e
Justin Mayfield <tooker@gmail.com>
2014-06-18 11:58:44 -0600

    Make PyPi long_description look pretty with pandoc (if available).

--- setup.py
+++ setup.py
@@ -2,15 +2,26 @@
 
 from setuptools import setup, find_packages
 
+README = 'README.md'
+
+def long_desc():
+    try:
+        import pypandoc
+    except ImportError:
+        with open(README) as f:
+            return f.read()
+    else:
+        return pypandoc.convert(README, 'rst')
+
 setup(
     name='syndicate',
-    version='0.99.6',
+    version='0.99.7',
     description='A wrapper for REST APIs',
     author='Justin Mayfield',
     author_email='tooker@gmail.com',
     url='https://github.com/mayfield/syndicate/',
     license='MIT',
-    long_description=open('README.rst').read(),
+    long_description=long_desc(),
     packages=find_packages(),
     install_requires=[
         'requests',
