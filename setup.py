a603d2567413bb02c7dec605ae0b433b8d9db321
Justin Mayfield <tooker@gmail.com>
2014-11-03 15:49:52 -0700

    Role version to 0.99.9 (for real)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def long_desc():
 
 setup(
     name='syndicate',
-    version='0.99.8',
+    version='0.99.9',
     description='A wrapper for REST APIs',
     author='Justin Mayfield',
     author_email='tooker@gmail.com',
